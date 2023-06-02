799adba9eff9557934e99f9ce23c9959859de357
jupeng <1662345805@qq.com>
2023-06-02 16:41:28 +0800

    test：netlify打包

--- src/App.vue
+++ src/App.vue
@@ -3,9 +3,9 @@
 // you can use this to manipulate the document head in any components,
 // they will be rendered correctly in the html results with vite-ssg
 useHead({
-  title: 'Vitesse',
+  title: 'AnthonyJu Blog',
   meta: [
-    { name: 'description', content: 'Opinionated Vite Starter Template' },
+    { name: 'description', content: '鞠鹏的个人博客网站' },
     {
       name: 'theme-color',
       content: () => isDark.value ? '#00aba9' : '#ffffff',
