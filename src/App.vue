94c648b5a60edf84a61727d6bb1ded75175197cb
jupeng <1662345805@qq.com>
2023-06-02 17:25:21 +0800

    feat：更换图标

--- src/App.vue
+++ src/App.vue
@@ -1,9 +1,11 @@
+<template>
+  <RouterView />
+</template>
+
 <script setup lang="ts">
 // https://github.com/vueuse/head
-// you can use this to manipulate the document head in any components,
-// they will be rendered correctly in the html results with vite-ssg
 useHead({
-  title: 'AnthonyJu Blog',
+  title: 'JuPeng\'s Blog',
   meta: [
     { name: 'description', content: '鞠鹏的个人博客网站' },
     {
@@ -11,16 +13,5 @@ useHead({
       content: () => isDark.value ? '#00aba9' : '#ffffff',
     },
   ],
-  link: [
-    {
-      rel: 'icon',
-      type: 'image/svg+xml',
-      href: () => preferredDark.value ? '/favicon-dark.svg' : '/favicon.svg',
-    },
-  ],
 })
 </script>
-
-<template>
-  <RouterView />
-</template>
--- vite.config.ts
+++ vite.config.ts
@@ -103,8 +103,8 @@ export default defineConfig({
       registerType: 'autoUpdate',
       includeAssets: ['favicon.svg', 'safari-pinned-tab.svg'],
       manifest: {
-        name: 'Vitesse',
-        short_name: 'Vitesse',
+        name: 'JuPeng\'s Blog',
+        short_name: 'JuPeng\'s Blog',
         theme_color: '#ffffff',
         icons: [
           {
