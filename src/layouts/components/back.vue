a3926df56ab207578237f10130d4ad7b939568c2
jupeng <1662345805@qq.com>
2023-06-07 17:23:28 +0800

    pref：根据文章类型返回上一页

--- src/layouts/components/back.vue
+++ src/layouts/components/back.vue
@@ -1,16 +1,34 @@
 <template>
   <div
-    v-if="showBack" class="my-20px inline-block cursor-pointer text-20px underline underline-offset-5"
+    v-if="isBlog || isNote"
+    class="my-20px inline-block cursor-pointer text-20px underline underline-offset-5"
     hover="text-$text-hover"
-    @click="$router.push('/blog')"
+    @click="goBack()"
   >
-    cd  . .
+    cd . .
   </div>
 </template>
 
 <script setup lang='ts'>
 const route = useRoute()
-const showBack = computed(() => {
-  return route.path.startsWith('/blog/') || route.path.startsWith('/note/')
+const router = useRouter()
+
+const isBlog = computed(() => {
+  return route.path.startsWith('/blog/')
 })
+const isNote = computed(() => {
+  return route.path.startsWith('/note/')
+})
+
+function goBack() {
+  if (isBlog.value) {
+    router.push('/blog')
+  }
+  else if (isNote.value) {
+    router.push('/note')
+  }
+  else {
+    router.push('/')
+  }
+}
 </script>
