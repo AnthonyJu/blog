bb7e2b21178960f0bb4f7806d1bec3fba10b59f2
jupeng <1662345805@qq.com>
2023-06-09 17:54:55 +0800

    pref：roast样式优化

--- src/pages/roast/index.vue
+++ src/pages/roast/index.vue
@@ -1,13 +1,14 @@
 <template>
   <div>
-    <div v-for="item in roastList" :key="item.id" flex>
-      <img class="w-64px rounded-50% shadow" border="~ gray-500" src="@/assets/avatar.png" alt="AnthonyJu">
-      <div class="roast-content ml-20px bg-red p-8px">
-        <p>
-          {{ item.content }}
-        </p>
-        <div class="flex-center-r text-14px">
-          <div i-carbon-calendar-heat-map mr-5px />
+    <div v-for="(item, index) in roastList" :key="item.id" mb-40px flex>
+      <img class="h-64px w-64px rounded-50% shadow" border="~ gray-500" src="@/assets/avatar.png" alt="AnthonyJu">
+      <div
+        class="roast-content ml-20px max-w-70% flex-col rounded px-8px py-4px text-#fff"
+        :style="{ 'background': colorList[index % 5], '--before-color': colorList[index % 5] }"
+      >
+        <p flex flex-1 items-center text-14px v-html="item.content" />
+        <div class="flex-center-r pt-4px text-14px" border="t dashed">
+          <div i-carbon-calendar-heat-map mr-5px text-14px />
           <span>{{ item.date }}</span>
         </div>
       </div>
@@ -17,34 +18,36 @@
 </template>
 
 <script setup lang="ts">
-import allRoastList from './roastList'
+import allRoasts from './roastList'
+
+const colorList = ['#f56a00', '#7265e6', '#ffbf00', '#00a2ae', ' #00a854']
 
 const current = ref(1)
 
 const roastList = computed(() => {
   const start = (current.value - 1) * 15
   const end = current.value * 15
-  return allRoastList.slice(start, end)
+  return allRoasts.slice(start, end)
 })
 </script>
 
 <style lang="scss" scoped>
-// .roast-content {
-//   position: relative;
+.roast-content {
+  position: relative;
 
-//   &::before {
-//     position: absolute;
-//     top: 0;
-//     left: 0;
-//     width: 0;
-//     height: 0;
-//     margin-top: -10px;
-//     margin-left: -10px;
-//     content: '';
-//     border: 10px solid transparent;
-//     border-right: 0;
-//     border-bottom: 0;
-//     border-left-color: red;
-//   }
-// }
+  &::before {
+    position: absolute;
+    inset: 0;
+    width: 0;
+    height: 0;
+    margin-top: 26px;
+    margin-left: -5px;
+    content: '';
+    border: 10px solid transparent;
+    border-right: 0;
+    border-bottom: 0;
+    border-left-color: var(--before-color);
+    transform: rotate(45deg);
+  }
+}
 </style>
