2d7ef5f4477354fc11d39a48ebec9d5ec26e8454
JuPeng <1662345805@qq.com>
2023-06-11 21:54:08 +0800

    pref：代码优化，依赖更新，暂不使用DM Sans字体

--- src/pages/index/components/left.vue
+++ src/pages/index/components/left.vue
@@ -23,7 +23,7 @@
   <div px-2px>
     <div class="mt-10px text-20px font-bold">Ju Peng</div>
     <div class="relative text-18px text-dark-300 -top-6px dark:text-gray-300">AnthonyJu</div>
-    <div mb-10px>巴山楚水凄凉地，Responsibility。</div>
+    <div mb-10px break-all>巴山楚水凄凉地，Responsibility。</div>
   </div>
 
   <!-- Btn -->
