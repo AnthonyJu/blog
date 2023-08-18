3fe01a34b5998ac81cdd1dbfb6a0fc42b199620e
JuPeng <1662345805@qq.com>
2023-08-18 20:48:07 +0800

    feat：首页适当进行适配

--- src/pages/index/components/things.vue
+++ src/pages/index/components/things.vue
@@ -1,5 +1,5 @@
 <template>
-  <div class="clip mt-20px text-right text-20px">随机文章</div>
+  <div class="clip pt-17px text-center text-18px">隔着人潮呼救像只困兽，呼吸颤抖去泪流，去相拥。</div>
   <div
     v-for="blog in blogs"
     :key="blog.path"
@@ -10,31 +10,36 @@
   >
     <!-- poster -->
     <img
-      class="h-160px w-200px"
+      class="h-160px"
+      :class="width > 600 ? 'w-210px' : 'w-45%'"
       object="cover"
       :src="blog.poster"
       :alt="blog.title"
     >
     <div class="flex-col-center flex-1 py-6px">
       <!-- title -->
-      <div class="my-10px" :title="blog.title">
+      <div class="my-10px">
         {{ blog.title }}
       </div>
       <!-- desc -->
-      <div class="flex-center flex-1 px-26px indent-2em" :title="blog.title">
+      <div
+        v-show="width > 600"
+        class="hidden-2 h-50px flex-center px-26px indent-2em"
+        :title="blog.desc"
+      >
         {{ blog.desc }}
       </div>
 
-      <div class="my-10px flex-center">
+      <div class="my-10px flex-center flex-wrap gap-15px">
         <!-- tags -->
-        <div class="flex-center gap-5px truncate" :title="blog.keywords?.join(',')">
+        <div class="flex-center gap-5px truncate">
           <el-tag v-for="tag in blog.keywords" :key="tag" round>
             {{ tag }}
           </el-tag>
         </div>
 
         <!-- date -->
-        <div class="ml-26px flex-center text-14px" :title="`编写于：${blog.date}`">
+        <div class="flex-center text-14px">
           <div i-carbon-calendar-heat-map mr-5px />
           <div>{{ blog.date }}</div>
         </div>
@@ -49,10 +54,10 @@ import pages from '~pages'
 
 const allBlogs = getAllBlogs(pages)
 
-// 随机获取3篇blog
+// 随机获取4篇blog
 const blogs = computed(() => {
   const randomBlogs: BlogInfo[] = []
-  while (randomBlogs.length < 3 && allBlogs.length > 0) {
+  while (randomBlogs.length < 4 && allBlogs.length > 0) {
     const index = Math.floor(Math.random() * allBlogs.length)
     const blog = allBlogs[index]
     if (!randomBlogs.includes(blog)) {
@@ -61,8 +66,17 @@ const blogs = computed(() => {
   }
   return randomBlogs
 })
+
+const { width } = useWindowSize()
 </script>
 
 <style lang='scss' scoped>
-
+// 超出两行省略
+.hidden-2 {
+  display: -webkit-box;
+  overflow: hidden;
+  text-overflow: ellipsis;
+  -webkit-line-clamp: 2; // 行数
+  -webkit-box-orient: vertical;
+}
 </style>
