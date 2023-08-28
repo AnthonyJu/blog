9493bdc4dcf6ecbb446c08e1b1db18f64c193926
JuPeng <1662345805@qq.com>
2023-08-28 20:49:06 +0800

    test

--- src/utils/checkUpdate.ts
+++ src/utils/checkUpdate.ts
@@ -5,10 +5,14 @@ export async function checkUpdate() {
   // 开发环境不检查更新
   if (import.meta.env.DEV) return
 
-  const { data } = await axios.get('/about', {
+  const { data } = await axios.get(`/about?time=${Date.now()}`, {
     responseType: 'text',
   })
+  // eslint-disable-next-line no-console
+  console.log('🚀 ~ file: checkUpdate.ts:10 ~ data:', data)
   const src = (document.querySelector('script[type="module"]') as HTMLScriptElement)?.src
+  // eslint-disable-next-line no-console
+  console.log('🚀 ~ file: checkUpdate.ts:13 ~ src:', src)
 
   if (!data.includes(src)) {
     // 创建一个倒计时组件
