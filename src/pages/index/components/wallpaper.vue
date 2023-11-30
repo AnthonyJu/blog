ebc23947f5cab4ada2ae6442b28185ffb52d297d
JuPeng <1662345805@qq.com>
2023-11-30 22:02:42 +0800

    Add image handling and error handling in wallpaper component

--- src/pages/index/components/wallpaper.vue
+++ src/pages/index/components/wallpaper.vue
@@ -8,13 +8,23 @@
         :key="index"
         class="banner"
         :src="`https://api.yimian.xyz/img?type=moe&size=1920x1080&index=${index}`"
+        @error="handleImgError"
       >
     </el-carousel-item>
   </el-carousel>
 </template>
 
 <script setup lang="ts">
+import img1 from '@/assets/wallpaper/1.jpg'
+import img2 from '@/assets/wallpaper/2.jpg'
+import img3 from '@/assets/wallpaper/3.jpg'
+import img4 from '@/assets/wallpaper/4.jpg'
 
+const imgList = [img1, img2, img3, img4]
+
+function handleImgError(e: any) {
+  e.target.src = imgList[e.target.src.split('index=')[1] - 1]
+}
 </script>
 
 <style lang="scss" scoped>
